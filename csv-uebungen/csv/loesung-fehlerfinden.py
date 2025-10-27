34d3a3255d877cb4e3c6b416139d6c01205106b7
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2025-10-27 08:41:55 +0100

    loesung fehlerfinden.py

---
+++ csv-uebungen/csv/loesung-fehlerfinden.py
@@ -0,0 +1,52 @@
+import csv
+
+# Achtung! Damit das Programm funktioniert, muss es in jenem Ordner ausgeführt werden,
+# indem die Datei 'fehler.csv' liegt. Ansonsten wird die Datei nicht gefunden.
+# Navigieren Sie dazu in der VS-Code Kommandozeile ins Verzeichnis 'csv-uebungen/csv'
+
+with open("fehler.csv") as csvfile:
+    # CSV-Format erraten
+    dialect = csv.Sniffer().sniff(csvfile.read(1024))
+    csvfile.seek(0)
+    # Datei mit erratenem Format einlesen
+    csv_reader = csv.DictReader(csvfile, dialect=dialect)
+    # Array mit allen Zeilen erstellen
+    tabelle = [row for row in csv_reader]
+
+header : list[str] = csv_reader.fieldnames  # type: ignore
+
+print(f"Header: {header}")
+print("\nTabelle als Liste von dictionaries:")
+print(tabelle)
+
+print("\nKonvertierung der Zahlen")
+for zeile in tabelle:
+    for spalte in header:
+        zeile[spalte] = int(zeile[spalte])  # Aus Zeichenketten Zahlen machen
+
+print(tabelle)
+
+print(f"\nZeile 3 (Index 2): tabelle[2]={tabelle[2]}")
+print(f"tabelle[2]['x'] = {tabelle[2]['x']},  tabelle[2]['x^3'] = {tabelle[2]['x^3']}")
+
+# TODO
+# Fehler finden, ausgeben und in der Tabelle korrigieren
+print("\n\nHier kommen die Fehler\n")
+for zeile in tabelle:
+    x = zeile['x']
+    for exponent in range(2,5):
+        korrekt = x ** exponent
+        if zeile[f"x^{exponent}"] != korrekt:
+            print(f"Fehler bei {zeile} exponent {exponent}")
+            print(f"Korrekt ist {korrekt}")
+            zeile[f"x^{exponent}"] = korrekt
+
+
+# CSV-Datei korrigiert schreiben
+with open("korrigiert.csv", "w") as csvfile:
+    # Writer vorbereiten
+    writer = csv.DictWriter(csvfile, fieldnames=header, dialect=dialect)
+    # Header (erste Zeile) schreiben
+    writer.writeheader()
+    # Alle Zeilen schreiben
+    writer.writerows(tabelle)
